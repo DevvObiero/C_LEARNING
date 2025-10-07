c714775426af29231d637c0c73398356574ae8c1
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-07 16:29:31 +0300

    learnign C

--- index.c
+++ index.c
@@ -8,8 +8,14 @@ int main(){
 
  float networth = 4000000;
 
+ char grade = 'A';
+
+ printf("My grades this year will average to an %c which transtaltes to a first class honors\n", grade);
+
  printf("In 10 years time my net worth will be $%.2f ",networth);
 
  return 0;
 }
+
+
 
