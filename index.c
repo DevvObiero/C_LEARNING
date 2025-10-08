700d2f955e1ad5e09491f95c51c69e14bc09a947
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-08 14:30:54 +0300

    don learnign about augmentend operators

--- index.c
+++ index.c
@@ -4,21 +4,10 @@
 
 int main(){
 
+    int age = 25;
+    printf("%5d", age);
 
-
-//  a variable is a resusable container of  value
-
-// in C we dont have stringss
-// we use an array fo characters 
-// learning about booleans
-
-bool isonline = false;
-printf("%d\n",isonline);
-
-char name[] = "Hello there ! My name is Paul Obiero";
-printf("Hello %s", name);
-
-return 0;
+    return 0; 
 }
 
 
