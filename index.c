018d3d63051c0662674b4d9b95fa6efd65aa9dc8
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-13 19:44:28 +0300

    first step implemented

--- index.c
+++ index.c
@@ -4,6 +4,17 @@ int main(){
 
 //  SHOPING CART PROGRAM 
 
-    return 0; 
+
+
+char item[50] = "";
+float price = 0.0f;
+int quantity = 0;
+char currency = "$";
+float total = 0.0f;
+
+printf("what item would you like to buy?:");
+printf("what is the price for each?");
+printf("How many would you like to buy?");
+return 0; 
 }
 
