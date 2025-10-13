4d95cb3e6638bc31dac449a5c50ffa96d689c42c
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-13 18:39:28 +0300

    learning aboutinput in c

--- index.c
+++ index.c
@@ -4,10 +4,21 @@
 
 int main(){
 
-    int age;
-    float gpa;
-    char grade;
-    char name[30];
+    int age = 0;
+    float gpa = 0.00;
+    char grade = '\0';
+    char name[30] = "";
+
+
+
+
+printf("%d\n", age);
+printf("%f\n", gpa);
+printf("%c\n", grade);
+printf("%s\n", name);
+
+
+
 
     return 0; 
 }
