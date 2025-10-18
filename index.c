c2643edef44ce8fea63fb03d132dd10ca4288105
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-18 13:06:32 +0300

    made cool statements

--- index.c
+++ index.c
@@ -2,22 +2,35 @@
 
 int main(){
 
-// #MAD LIBS GAME 
+// #MAD LIBS GAME
+
+char noun[50] = "";
+char verb[50] = "";
+char adjective1[50] = "";
+char adjective2[50] = "";
+char adjective3[50]="";
+
+printf("Enter a noun: ");   
+fgets(noun, sizeof(noun), stdin);
 
 
+printf("Enter a verb: ");
+fgets(verb, sizeof(verb), stdin);
+
+printf("Enter an adjective: ");
+fgets(adjective1, sizeof(adjective1), stdin);
+
+printf("Enter another adjective: ");
+fgets(adjective2, sizeof(adjective2), stdin);
+
+printf("Enter one more adjective: ");
+fgets(adjective3, sizeof(adjective3), stdin);
+
+printf("Here is your story:\n");
 
 
+printf("Once upon a time, there was a %s %s who loved to %s. Every day, the %s %s would embark on %s adventures.\n", adjective1, noun, verb, adjective2, noun, adjective3);
 
-
-
-            char name[] = "Paul Obiero";
-            char age[] = "20";
-            char height[] = "5,6 feet";
-
-            printf("There once was a person named %s\n", name);
-            printf("He was %s years old\n", age);
-            printf("He was %s tall\n", height);
-            printf("He  really lived the bright and succesful life he always wanted");
 
 return 0; 
 }
