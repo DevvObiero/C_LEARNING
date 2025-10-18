d33ea5681a2a191aef3281f7e0bafbeb54ad1bdb
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-18 12:00:14 +0300

    mad libsgame

--- index.c
+++ index.c
@@ -2,31 +2,9 @@
 
 int main(){
 
-//  SHOPING CART PROGRAM 
+// #MAD LIBS GAME 
 
 
-
-char item[50] = "";
-float price = 0.0f;
-int quantity = 0;
-char currency = '$';
-float total = 0.0f;
-
-printf("what item would you like to buy?:");
-fgets(item, sizeof(item), stdin);
-
-
-printf("what is the price for each?");
-scanf("%f", &price);
-
-
-printf("How many would you like to buy?");
-scanf("%d", &quantity);
-
-total = price * quantity;
-
-
-printf("You have purchased %d of %s at a price of %.2f%c each for a total of %.2f%c\n", quantity, item, price, currency, total, currency);
 return 0; 
 }
 
