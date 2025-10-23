55bc71ad1964fcd1c16bf8b8777005cd2e80eac3
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-24 00:04:57 +0300

    sqrt

--- index.c
+++ index.c
@@ -1,27 +1,13 @@
 #include <stdio.h>
+#include <math.h>>
+
 
 int main() {
-int a,b;
-    char z;
-    // a = total purchase
-    // b = age
-    // z = gender
 
-    printf("Enter purchase amount:");
-    scanf("%d", &a);
-    printf("Enter your age:");
-    scanf("%d", &b);
-    printf("Enter your gender, M or F:");
-    scanf(" %c",&z);
+int x = 9;
+printf("The square root of %d is %.2f\n", x, sqrt(x));
 
-    int criteria1 = (a >= 10000);
-    int criteria2 = ((b >= 70) && (a >= 5000));
-    int criteria3 = ((z == 'F' || z == 'f') && (b >= 50));
-    int e = criteria1 || criteria2 || criteria3;
-    // d = discount granted
-    
-    printf("Result %s\n",
-        e ? "Discount Granted" : "Discount Denied");
-    
+
+
     return 0;
 }
