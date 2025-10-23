f7368a9041491c8c77c62abda4587766d0d86e2a
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-24 00:25:05 +0300

    area calculation

--- index.c
+++ index.c
@@ -4,10 +4,15 @@
 
 int main() {
 
-int x = 9;
-printf("The square root of %d is %.2f\n", x, sqrt(x));
+    double radius = 0.0;
+    double area = 0.0;
+    const PI = 3.141592653589793;
 
+    printf("Enter the radius of the circle: ");
+    scanf("%lf",&radius);
 
+    area = PI * pow(radius, 2);
+    printf("The area of the circle with radius %.2f is %.2f\n", radius, area);
 
     return 0;
 }
