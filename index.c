a796b67ce5e1b455862d7c1dcb4a009f6af830ff
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-10-26 12:45:01 +0300

    yessir

--- index.c
+++ index.c
@@ -1,31 +1,18 @@
 #include <stdio.h>
-#include <math.h>
 
 
 int main() {
-double  principle = 0.0;
-double rate = 0.0 ;
-int years = 0 ;
-int timesCompounded = 0 ;
-double  totalAmount = 0.0;
-double  interest = 0.0;
+    int age;
+printf("Enter your age: ");
+scanf("%d", &age);
 
-printf("Enter the principle amount: ");
-scanf("%lf",&principle);
-
-printf("Enter the rate of interest: ");
-scanf("%lf,",&rate);
-rate = rate / 100;
-
-interest = principle * rate;
-
-printf("Enter the number of years: ");
-scanf("%d",&years);
-printf("Enter the number of times the interest is compounded per year: ");
-scanf("%d",&timesCompounded);
-totalAmount = principle * pow((1 + rate / timesCompounded), timesCompounded * years);
-printf("The total amount after %d years is: $%.2lf\n", years, totalAmount);
-printf("The interest on the principle amount is: $%.2lf\n", interest);
+    if (age > 21)
+    {
+        printf("You are eligible to vote  nad drink alcohol.\n");
+    }else
+    {
+        printf("You are not eligible to vote and drink alcohol.\n");
+    }
 
 
 
