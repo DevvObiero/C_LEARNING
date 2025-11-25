1245aee3f3fde669e4d8df76265585891a003b1a
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-11-25 12:53:03 +0300

    ads

--- index.c
+++ index.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
-#include <string.h>
+// global variable
+// int globalVariable = 10;
+ // function porotototype global declaration
+// int add(int x, int y);
 
+// int main() {
+
+//     int a = 5, b = 15;
+//     int sum = add(a, b);
+//     printf("Sum: %d\n", sum);
+//     printf("Global Variable: %d\n", globalVariable);
+
+//     return 0;
+// }
+ // function definition 
+// int add(int x, int y) {
+//     return x + y + globalVariable;
+// }
+
+#include<stdio.h>
 int main() {
-    
-int choice  = 0 ; 
-float pounds = 0.0f ;
-float kilograms = 0.0f ;
-
-printf("Select an option:\n");
-printf("1. Convert Pounds to Kilograms\n");
-printf("2. Convert Kilograms to Pounds\n");
-printf("Enter your choice (1 or 2): ");
-scanf("%d", &choice);
-if (choice == 1) {
-    printf("Enter weight in Pounds: ");
-    scanf("%f", &pounds);
-    kilograms = pounds * 0.45359237f;
-    printf("%.2f Pounds is equal to %.2f Kilograms\n", pounds, kilograms);
-} else if (choice == 2) {
-    printf("Enter weight in Kilograms: ");
-    scanf("%f", &kilograms);
-    pounds = kilograms / 0.45359237f;
-    printf("%.2f Kilograms is equal to %.2f Pounds\n", kilograms, pounds);
-} else {
-    printf("Invalid choice. Please select 1 or 2.\n");
-}
-
-
-
-return 0;
+    int a = 34;
+    float b = 45.985;
+    float sum = a + b;
+    printf("The sum is %f/n" , sum);
+    return 0;
 }
