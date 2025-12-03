7a98db9e003505bdc726bdd2602c29d06ee7b238
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-12-04 00:11:46 +0300

    learnign about fgets

--- index.c
+++ index.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+
+
+
 
 int main(){
 
 
-int age = 0;
-printf("Enter your age: ");
-scanf("%d", &age);
+char name[50]="";
+printf("Enter your name: ");
+fgets(name, sizeof(name), stdin);
+name[strcspn(name, "\n")] = 0;  // Remove newline character if present
 
 
-if(age >= 18 ){
-    printf("you are an adult");
-}else if(age == 0){
-    printf("You are not born yet");
-}else{
-  printf("You are a child");
+if(strlen(name) == 0){
+    printf("No name entered.\n");
+} else {
+    printf("Hello, %s!\n", name);
 }
 
+return 0;
 
 
 
-  return 0;
 }
