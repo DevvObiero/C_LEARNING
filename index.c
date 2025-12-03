2bacc908bd101c24826b0d2a0db7bc0a47fd7487
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-12-03 20:54:18 +0300

    learning C - If Else Statement

--- index.c
+++ index.c
@@ -3,10 +3,12 @@
 int main(){
 
 
-int age = 20;
+int age = 0;
 
 if(age >= 18 ){
     printf("you are an adult");
+}else if(age == 0){
+    printf("You are not born yet");
 }else{
   printf("You are a child");
 }
